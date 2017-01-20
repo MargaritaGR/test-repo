70d685efc539ce760f84ad8b5bc9da540c1fcfdd
Margarita Radeva <magi_rrr@yahoo.com>
2017-01-20 21:55:42 +0200

    1..100

---
+++ 1..100/1..100/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1._._100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool b = true;
+
+            while (b)
+            {
+                Console.Write("Enter a number in the range [1..100]:");
+                var n = int.Parse(Console.ReadLine());
+                if ((n >= 1) && (n <= 100))
+                {
+                    Console.WriteLine("The number is: " + n);
+                    b = false;
+                }
+                else
+                    Console.WriteLine("Invalid number!");
+                
+                
+            }
+
+
+        }
+    }
+}
